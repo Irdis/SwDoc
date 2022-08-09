56be0e9a59f5f536df42823f21ba9a9648bf6ad7
Ivan Novitskii <ivannovitskii@gmail.com>
2022-08-09 18:23:29 +0300

    fix grammar

--- In.SwDoc/ClientApp/src/app/home/generation-result.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GenerationResult = /** @class */ (function () {
-    function GenerationResult() {
-    }
-    return GenerationResult;
-}());
-exports.GenerationResult = GenerationResult;
-//# sourceMappingURL=generation-result.js.map
